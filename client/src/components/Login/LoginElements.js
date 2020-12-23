1eaf5e091e52572de076be2597c20429d10a0581
Ahniya Payne <ahniyap9231@gmail.com>
2020-12-23 02:09:27 -0600

    add minimal styling to login page

--- client/src/components/Login/LoginElements.js
+++ client/src/components/Login/LoginElements.js
@@ -11,7 +11,7 @@ import styled from "styled-components";
 // `
 
 export const LoginContainer = styled.div`
-  background-color: grey;
+  background-color: #e0e0e0;
   width: 100vw;
   height: 100vh;
 `;
@@ -19,12 +19,46 @@ export const LoginContainer = styled.div`
 export const LoginBox = styled.div`
   background-color: white;
   position: absolute;
-  width: 500px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 800px;
   height: 550px;
   top: 15vh;
-  left: 34vw;
+  left: 25vw;
   margin: 0 auto;
   border: none;
   border-radius: 4px;
+  -webkit-box-shadow: 9px 5px 18px 0px rgba(50, 50, 50, 0.28);
+  -moz-box-shadow: 9px 5px 18px 0px rgba(50, 50, 50, 0.28);
+  box-shadow: 9px 5px 18px 0px rgba(50, 50, 50, 0.28);
   /* outline: 1px solid red; */
+`;
+
+export const LoginForm = styled.form`
+  grid-column-start: 2;
+  grid-column-end: 3;
+  font-size: 3rem;
+  padding: 20px;
+`;
+
+export const ColorBlock = styled.div`
+  background-color: #ffeb3b;
+  grid-column-start: 1;
+  grid-column-end: 2;
+`;
+
+export const InputLabel = styled.label`
+  display: block;
+  font-size: 1rem;
+  color: grey;
+  margin-bottom: -1.5rem;
+`;
+
+export const InputField = styled.input`
+  font-size: 1.3rem;
+  background-color: lightgray;
+  color: #000;
+  border: none;
+  border-radius: 3px;
+  height: 30px;
 `;
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { LoginBox, LoginContainer } from "components/Login/LoginElements";
+import {
+  LoginBox,
+  LoginContainer,
+  LoginForm,
+  ColorBlock,
+  InputField,
+  InputLabel,
+} from "components/Login/LoginElements";
 
 export default function LoginPage() {
   return (
     <>
       <LoginContainer>
         <LoginBox>
-          <form action="http://localhost:5000/auth/login" method="post">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+          <ColorBlock />
+          <LoginForm action="http://localhost:5000/auth/login" method="post">
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <InputField type="text" name="email" id="email" />
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <InputField type="password" name="password" id="password" />
             <input type="submit" id="submit" value="Submit" />
-          </form>
+          </LoginForm>
         </LoginBox>
       </LoginContainer>
     </>
